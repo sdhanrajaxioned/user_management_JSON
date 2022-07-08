fd61f8dfe57018972e9647abc583acaab6877fbd
sdhanrajaxioned <dhanrajs@axioned.com>
2022-07-08 08:03:48 +0530

    addde getdata and save data function and renamed users.json

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const port = 3000;
 
-const usersData = fs.readFileSync('database.json')
-const userJsonData = JSON.parse(usersData);
-console.log(userJsonData);
+//read data from json file
+const getUsersData = () => {
+  const jsonData = fs.readFileSync('users.json');
+  return JSON.parse(jsonData);
+}
+
+//writing data to json file
+const saveUserData = (data) => fs.writeFileSync('users.json', JSON.stringify(data));
+
+app.listen(port, console.log(`listening on ${port} ...`))
